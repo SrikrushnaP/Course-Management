48ebefd27269ebf7e5a0118b4002f72cbf70a859
Srikrushna <palsrikrushna6@gmail.com>
2024-06-20 03:14:25 +0530

    - Moved nav to a separate component - Created simple home page

--- Course-Management-Frontend/src/app/app.component.ts
+++ Course-Management-Frontend/src/app/app.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { HomeComponent } from "./components/home/home.component";
+import { NavComponent } from "./components/nav/nav.component";
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, FontAwesomeModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, FontAwesomeModule, HomeComponent, NavComponent]
 })
 export class AppComponent {
   title = 'Course-Management-Frontend';
