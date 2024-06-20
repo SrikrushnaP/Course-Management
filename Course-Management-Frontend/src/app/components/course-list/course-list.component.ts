0e2479905590c843135c478530ea99dfcadfe8ff
Srikrushna <palsrikrushna6@gmail.com>
2024-06-20 20:15:57 +0530

    Added FontAwesome icon in list page

--- Course-Management-Frontend/src/app/components/course-list/course-list.component.ts
+++ Course-Management-Frontend/src/app/components/course-list/course-list.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faTrash,faPenToSquare } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-course-list',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FontAwesomeModule],
   templateUrl: './course-list.component.html',
   styleUrl: './course-list.component.css'
 })
 export class CourseListComponent {
-
+  faTrash = faTrash;
+  faPenToSquare = faPenToSquare;
 }
