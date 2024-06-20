470e9f08b9462f99d3c6f33130e4ecbe0077fa62
Srikrushna <palsrikrushna6@gmail.com>
2024-06-20 19:55:31 +0530

    Add Routes to the Application - Import the necessary component - Setting up the routes array - Route parameters - Configuring the routes in all our components - Modifed the style for active and hover link in nav

--- Course-Management-Frontend/src/app/app.component.ts
+++ Course-Management-Frontend/src/app/app.component.ts
@@ -1,17 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faCoffee } from '@fortawesome/free-solid-svg-icons';
-import { HomeComponent } from "./components/home/home.component";
 import { NavComponent } from "./components/nav/nav.component";
-import { RegisterComponent } from "./components/register/register.component";
-import { LoginComponent } from './components/login/login.component';
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, FontAwesomeModule, HomeComponent, NavComponent, RegisterComponent, LoginComponent]
+    imports: [RouterOutlet, RouterLink, FontAwesomeModule, NavComponent]
 })
 export class AppComponent {
   title = 'Course-Management-Frontend';
--- Course-Management-Frontend/src/app/app.routes.ts
+++ Course-Management-Frontend/src/app/app.routes.ts
@@ -1,3 +1,26 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { CreateCourseComponent } from './components/create-course/create-course.component';
+import { EditCourseComponent } from './components/edit-course/edit-course.component';
+import { CourseListComponent } from './components/course-list/course-list.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'create-course',
+    component: CreateCourseComponent,
+  },
+  {
+    path: 'edit-course/:id',
+    component: EditCourseComponent,
+  },
+  {
+    path: 'list-course',
+    component: CourseListComponent,
+  },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
