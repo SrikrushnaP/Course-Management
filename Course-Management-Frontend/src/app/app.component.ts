72ca85ddf936d4ec1c900969d83221f85dc25366
Srikrushna <palsrikrushna6@gmail.com>
2024-06-20 18:27:05 +0530

    Designing and Adding Forms to our Registration Page What we did: Step 1: Generate the register component(ng generate component components/register) Step 2: Design the register component Step 3: Add styling to the register component Step 4: Checked the output by adding in app component

--- Course-Management-Frontend/src/app/app.component.ts
+++ Course-Management-Frontend/src/app/app.component.ts
@@ -4,12 +4,13 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 import { HomeComponent } from "./components/home/home.component";
 import { NavComponent } from "./components/nav/nav.component";
+import { RegisterComponent } from "./components/register/register.component";
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, FontAwesomeModule, HomeComponent, NavComponent]
+    imports: [RouterOutlet, FontAwesomeModule, HomeComponent, NavComponent, RegisterComponent]
 })
 export class AppComponent {
   title = 'Course-Management-Frontend';
