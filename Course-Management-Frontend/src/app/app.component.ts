15a287918dc566e2ce1dd4729731282bdb38bf45
Srikrushna <palsrikrushna6@gmail.com>
2024-06-20 18:32:24 +0530

    Designing and Adding Forms to our Login Page What we did: Step 1: Generate the login component(ng g c components/login) Step 2: Design the login component Step 3: Add styling to the login component Step 4: Checked the output by adding in app component

--- Course-Management-Frontend/src/app/app.component.ts
+++ Course-Management-Frontend/src/app/app.component.ts
@@ -5,12 +5,13 @@ import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 import { HomeComponent } from "./components/home/home.component";
 import { NavComponent } from "./components/nav/nav.component";
 import { RegisterComponent } from "./components/register/register.component";
+import { LoginComponent } from './components/login/login.component';
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, FontAwesomeModule, HomeComponent, NavComponent, RegisterComponent]
+    imports: [RouterOutlet, FontAwesomeModule, HomeComponent, NavComponent, RegisterComponent, LoginComponent]
 })
 export class AppComponent {
   title = 'Course-Management-Frontend';
