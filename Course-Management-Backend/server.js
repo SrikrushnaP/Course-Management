6b0db2345316239fef5a2dc092abd569e8ed49a0
Srikrushna <palsrikrushna6@gmail.com>
2024-06-22 00:58:48 +0530

    Setting up Mongo DB - Setup Schemas and Models for Users and Course Collection - Connect to MongoDB Database Using Mongoose

--- Course-Management-Backend/server.js
+++ Course-Management-Backend/server.js
@@ -1,6 +1,8 @@
 // Starting up the server
 const express = require('express')
 const morgan = require('morgan')
+const mongoose = require('mongoose');
+const dotenv = require('dotenv')
 const cors = require('cors')
 const notFound = require('./middlewares/notFound')
 const serverError = require('./middlewares/serverError')
@@ -8,6 +10,15 @@ const app = express()
 
 // Setting up the port
 const PORT = process.env.PORT || 5000
+
+//Allow access to .env file
+dotenv.config()
+
+//Database connection
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => console.log('Connected to Mongo DB'))
+    .catch (error => console.log(error));
 
 app.use(cors())
 app.use(express.json())
@@ -19,8 +30,8 @@ app.get('/', function (req, res) {
 
 app.get('/html-text', function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('' +
-    `<html>
+    res.end('' +
+        `<html>
     <head></head>
     <body>
     <h2>Welcome to the MEAN stack course</h2>
@@ -32,7 +43,7 @@ app.get('/html-text', function (req, res) {
       </script>
     </body>
     </html>`
-  )
+    )
 });
 
 //Invalid Route Error Handler
@@ -43,4 +54,4 @@ app.use(serverError)
 
 app.listen(PORT, () => {
     console.log(`Server Running on Port ${PORT}`)
-  })
+})
