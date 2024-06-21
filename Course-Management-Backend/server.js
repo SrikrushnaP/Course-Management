02ea6180876141e84e39abd1bd4a590b480d4e94
Srikrushna <palsrikrushna6@gmail.com>
2024-06-21 19:20:54 +0530

    Added middlewares - CORS and Morgan Middleware - Created Error Handler Middleware - Added few dependencies using below npm comand  - npm i morgan  - npm i cors

--- Course-Management-Backend/server.js
+++ Course-Management-Backend/server.js
@@ -1,9 +1,23 @@
 // Starting up the server
 const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const notFound = require('./middlewares/notFound')
+const serverError = require('./middlewares/serverError')
 const app = express()
 
 // Setting up the port
 const PORT = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json())
+app.use(morgan("common"))
+
+//Invalid Route Error Handler
+app.use(notFound);
+
+//Server Error Middleware Handler
+app.use(serverError)
 
 app.listen(PORT, () => {
     console.log(`Server Running on Port ${PORT}`)
