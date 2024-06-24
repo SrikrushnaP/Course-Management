33f70c7a35c0578a5a42b045f7db378383990532
Srikrushna <palsrikrushna6@gmail.com>
2024-06-24 21:02:43 +0530

    Implimented register user functionality - Create FormBuilder and Validators for the Register Page - Consuming REST API for the Register Page

--- Course-Management-Frontend/src/app/components/register/register.component.ts
+++ Course-Management-Frontend/src/app/components/register/register.component.ts
@@ -1,13 +1,57 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink, Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
+  providers: [UserService],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
 
+  constructor(private router: Router, private formBuilder: FormBuilder, private user_service: UserService) {
+    this.registerForm = this.formBuilder.group({
+      email: formBuilder.control('', [
+        Validators.required,
+        Validators.minLength(5),
+        Validators.pattern(
+          /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        ),
+      ]),
+      password: formBuilder.control('', [
+        Validators.required,
+        Validators.minLength(7),
+      ]),
+    });
+  }
+  //Get Form Controls
+  public control(name: string) {
+    return this.registerForm.get(name);
+  }
+
+  //Submit Form
+  public onSubmit() {
+    this.user_service.registerUser(this.registerForm.value).subscribe({
+      next: (res: any) => {
+        //response
+        console.log(res);
+        localStorage.setItem('token', res.token);
+        this.router.navigate(['/list-course']);
+      },
+      error: (error) => {
+        // handle error
+        console.log(error);
+      },
+      complete: () => {
+        console.log('Request complete');
+      },
+    });
+  }
+
+  ngOnInit() {}
 }
---
+++ Course-Management-Frontend/src/app/services/user.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  // private baseURL = 'https://coursemgtapp-ab00832e8359.herokuapp.com';
+  private baseURL = 'http://localhost:5000';
+
+  constructor(private http: HttpClient) {}
+
+  registerUser(user: any) {
+    return this.http.post(`${this.baseURL}/api/register`, user);
+  }
+}
